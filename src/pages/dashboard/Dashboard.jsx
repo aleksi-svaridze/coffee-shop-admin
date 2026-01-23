3cf3c3b144f7ee4b02b2d78e0edf06624b8ce786
Natalia Kavtaradze <nataliakavtaradze777@gmail.com>
2026-01-23 18:19:16 +0400

    add CoffeeDetails component and module css file

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
 import Header from "../../components/header/Header";
-<<<<<<< HEAD
-import styles from "./Dashboard.module.css";
-=======
 import Table from "../../components/table/Table";
->>>>>>> d6faa73608e65780296b68458b90c411bffa28b0
+import CoffeeCard from "../../components/coffeeCard/CoffeeCard";
+import styles from "./Dashboard.module.css";
 
 function Dashboard() {
   const [data] = useState([
@@ -64,70 +62,6 @@ function Dashboard() {
       />
 
       {/* Table */}
-<<<<<<< HEAD
-      <section className={styles.tableSection}>
-        <table className={styles.table}>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Origin</th>
-              <th>Caffeine</th>
-              <th>Price</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Ethiopian Yirgacheffe</td>
-              <td>Ethiopia</td>
-              <td>120mg</td>
-              <td>$4.99</td>
-              <td className={styles.actions}>
-                <button className={styles.view}>View</button>
-                <button className={styles.edit}>Edit</button>
-                <button className={styles.delete}>Delete</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </section>
-      <h2>Coffee-Cards</h2>
-
-      {/* Coffee Cards */}
-      <section className={styles.cards}>
-        <div className={styles.card}>
-          <img
-            src="https://images.unsplash.com/photo-1509042239860-f550ce710b93"
-            alt="coffee"
-          />
-          <h3>Ethiopian Yirgacheffe</h3>
-          <p>
-            A light roasted coffee with bright acidity, and complex fruit and
-            floral notes....
-          </p>
-          <p>
-            <strong>Origin:</strong>
-            "Ethiopia"
-          </p>
-          <p>
-            <strong>Caffeine:</strong>
-            "120mg"
-          </p>
-          <p>
-            <strong>Price:</strong>
-            "$4.99"
-          </p>
-          <div className={styles.card_footer}>
-            <button className={styles.view}>View More</button>
-            <button className={styles.edit}>Edit</button>
-            <button className={styles.delete}>Delete</button>
-          </div>
-        </div>
-      </section>
-=======
       <Table
         value_1={"ID"}
         value_2={"Name"}
@@ -139,7 +73,7 @@ function Dashboard() {
         editCoffee={editCoffee}
       />
       {/* Coffee Cards  */}
->>>>>>> d6faa73608e65780296b68458b90c411bffa28b0
+      <CoffeeCard />
     </main>
   );
 }
