99df97ef220e737ef7b8ed555d58f83251f1fd55
aleksi-svaridze <aleksi.svaridze82@gmail.com>
2026-01-23 09:09:48 +0400

    Add item to dashboard table

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -25,6 +25,13 @@ function Dashboard() {
       caffeine: "125mg",
       price: "$5.99",
     },
+    {
+      id: "cof_sample4",
+      name: "Kenyan BB",
+      origin: "Kenya",
+      caffeine: "725mg",
+      price: "$9.99",
+    },
   ]);
 
   const deleteCoffee = (id) => {
